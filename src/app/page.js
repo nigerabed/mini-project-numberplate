4095dd8d7d27b3c0dc78bad8561aa4497df5bc1f
nigerabed <nigerabed@gmail.com>
2025-08-16 08:24:48 +0200

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div >
+      <main >
+      
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
